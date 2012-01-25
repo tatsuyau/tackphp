24abb5237304e5e99762342b5657d3c862885ce6
tatsuya uehara <tatsuya.acdc@gmail.com>
2012-01-26 01:27:27 +0900

    transaction

--- model/Model.php
+++ model/Model.php
@@ -1,5 +1,25 @@
 <?php
 class Model{
-	public function __construct(){
-	}
+        protected $db;
+        public function __construct(){
+                $this->db = new database();
+        }
+
+        public function beginTransaction(){
+                $sql = 'SET AUTOCOMMIT = 0 ';
+                $this->db->execQuery($sql);
+
+                $sql = 'BEGIN ';
+                $this->db->execQuery($sql);
+        }
+
+        public function commit(){
+                $sql = 'COMMIT ';
+                $this->db->execQuery($sql);
+        }
+
+        public function rollback(){
+                $sql = 'ROLLBACK ';
+                $this->db->execQuery($sql);
+        }
 }
